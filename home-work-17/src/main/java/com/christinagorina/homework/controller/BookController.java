434395a76e18795587805145da53b91716db6ef0
christina-gorina <christina.gorina@yandex.ru>
2021-09-01 19:00:25 +0300

    home-work-17 - отправка на проверку

---
+++ home-work-17/src/main/java/com/christinagorina/homework/controller/BookController.java
@@ -0,0 +1,14 @@
+package com.christinagorina.homework.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BookController {
+
+    @GetMapping("/")
+    public String listPage() {
+        return "bookpage";
+    }
+
+}
